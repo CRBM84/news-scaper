d00ccad4c32d8b611e1e8cedf26445d0e28c1a21
CRBM84 <carlosrbock@gmail.com>
2025-08-28 17:09:50 +0200

    update expected imports

--- src/hacker-news/hacker-news.service.ts
+++ src/hacker-news/hacker-news.service.ts
@@ -1,10 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, HttpException, HttpStatus } from '@nestjs/common';
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsageLog } from './entities/usage-log.entity';
+import { Post } from './entities/post.entity';
 
 @Injectable()
 export class HackerNewsService {
 
+    //add helper methods here
 
 
+
+    //service methods for endpoints
     async fetchNews() {
 
     }
