c7f20b0a00fe5d924f0b6f88d7c4f2548d44516b
CRBM84 <carlosrbock@gmail.com>
2025-08-28 13:12:01 +0200

    chore: update app module with orm additional module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HackerNewsModule } from './hacker-news/hacker-news.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'data.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    HackerNewsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
