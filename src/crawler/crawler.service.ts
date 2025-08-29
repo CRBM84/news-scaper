dd15e49b2072135a58ea33c91450ba6813715817
CRBM84 <carlosrbock@gmail.com>
2025-08-29 11:55:06 +0200

    add helper methods to ensure recent posts are returned

--- src/crawler/crawler.service.ts
+++ src/crawler/crawler.service.ts
@@ -38,14 +38,13 @@ export class CrawlerService {
         posts.push({ rank, title, points, comments, createdAt: new Date() });
 
       })
-      console.log(posts);
 
-      //TODO: save to local db for caching.
-      //TODO: create helper method for freshness
-      //TODO: call usage-log service in each method
-      //TODO: define usage-long method/s
+      const thirtyPosts = posts.slice(0, 30);
+      const savedEntries = await this.postRepository.save(thirtyPosts);
 
-      return {};
+      this.logger.log('Saved ' + savedEntries.length + ' new entries to local DB');
+
+      return savedEntries;
     } catch (error) {
       this.logger.error('Error fetching data, please try again latter', error);
       throw new HttpException(
@@ -55,7 +54,32 @@ export class CrawlerService {
     }
   }
 
+  private isDataStale(lastestPost: Post): boolean {
+    const FIVE_MINUTES = 5 * 60 * 1000;
+    return Date.now() - lastestPost.createdAt.getTime() > FIVE_MINUTES;
+  }
+
+  private async getRecentArticles(): Promise<Post[]> {
+    try {
+      let posts = await this.postRepository.find({ order: { createdAt: 'DESC' }, take: 30 });
+      if (posts.length || this.isDataStale(posts[0])) {
+        this.logger.warn('No data or data is stale, fetching new data');
+        await this.fetchNews();
+        posts = await this.postRepository.find({ order: { createdAt: 'DESC' }, take: 30 });
+      }
+      return posts;
+    } catch (error) {
+      this.logger.error('Failed to fetch posts from db and Hacker News', error);
+      throw new HttpException('Data unavailable', HttpStatus.SERVICE_UNAVAILABLE)
+    }
+  }
+
   async fetchLongTitles() { }
 
   async fetchShortTitles() { }
+
+
+  //TODO: create helper method for freshness
+  //TODO: call usage-log service in each method
+  //TODO: define usage-long method/s
 }
